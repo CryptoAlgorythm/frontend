18d3a19509c865ec42fe33ecf2925d9f4ebbf284
cryptoalgorythm <cryptoalgorythm@gmail.com>
2018-07-09 01:54:21 -0400

    added index file to load App component and fun the first saga

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { render } from 'react-dom'
+import App from './App';
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { Provider } from 'react-redux';
+import { reducer } from './reducers';
+import { watcherSaga } from './sagas'
+import sagaMiddlewareFactory from 'redux-saga';
+
+// create saga middleware
+const createSagaMiddleware = createSagaMiddleware();
+
+// dev tools middlware
+const reduxDevTools =
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+// create a redux store with our reducer above and middleware
+let store = createStore(
+    reducer,
+    compose(applyMiddleware(sagaMiddleware), reduxDevTools)
+)
+
+// run the saga
+sagaMiddleware.run(watcherSaga);
+
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
+)
