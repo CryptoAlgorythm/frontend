fd5f746b3536242d802b52a3581e625366813ac6
cryptoalgorythm <cryptoalgorythm@gmail.com>
2018-07-09 02:12:58 -0400

    test that there is a button to upload a file

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -24,4 +24,18 @@ describe('App', () => {
     it('should have an input field to upload file', () => {
         expect(wrapper.find('input[type="file"]').length).toBe(1);
     })
+
+    it('should display file selector after file input field clicked', () => {
+        wrapper.find('input[type="file"]').simulate('change', {
+            target: {
+                files: [
+                    'data.txt'
+                ]
+            }
+        })
+    })
+
+    it('should have a button to upload file', () => {
+        expect(wrapper.find('button').length).toBe(1);
+    })
 })
