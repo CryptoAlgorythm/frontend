f687a5f4cce5ae5f40368c280d8a1e27e6d2c8f8
cryptoalgorythm <cryptoalgorythm@gmail.com>
2018-07-09 02:52:19 -0400

    api call success action creator works

--- __tests__/actions.test.js
+++ __tests__/actions.test.js
@@ -10,4 +10,24 @@ describe('actions', () => {
         }
         expect(actions.apiCallRequest(payload)).toEqual(expectedAction)
     });
+    it('should indicate successful upload', () => {
+        // const payload = 'data.txt'
+        const payload = {
+            0: { name: "hello", total: 3 },
+            1: { name: "there", total: 1 },
+            2: { name: "is", total: 1 },
+            3: { name: "no", total: 1 },
+            4: { name: "better", total: 1 },
+            5: { name: "way", total: 1 },
+            6: { name: "to", total: 1 },
+            7: { name: "say", total: 1 },
+            8: { name: "hello!", total: 1 },
+            9: { name: "", total: 1 }
+        }
+        const expectedAction = {
+            type: types.API_CALL_SUCCESS,
+            payload: payload
+        }
+        expect(actions.apiCallSuccess(payload)).toEqual(expectedAction)
+    })
 })
--- actions/index.js
+++ actions/index.js
@@ -4,3 +4,8 @@ export const apiCallRequest = (data) => ({
     type: actions.API_CALL_REQUEST,
     payload: data
 })
+
+export const apiCallSuccess = (data) => ({
+    type: actions.API_CALL_SUCCESS,
+    payload: data
+})
