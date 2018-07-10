b1667e287cf5f6c333cf7d65dfb1bf2298e9be71
cryptoalgorythm <cryptoalgorythm@gmail.com>
2018-07-10 02:00:59 -0400

    fixed app loading test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -16,10 +16,11 @@ describe('App', () => {
         wrapper = mount(<Provider store={store}><App /></Provider>);
     });
 
-    // it('App renders without crashing', () => {
-    //     let wrapper = shallow(<App />);
-    //     expect(wrapper.length).toEqual(1)
-    // })
+    it('App renders without crashing', () => {
+        let wrapper = shallow(<App />, { context: { store: mockStore() } });
+        expect(wrapper.length).toEqual(1)
+    })
+
 
     it('should have an input field to upload file', () => {
         expect(wrapper.find('input[type="file"]').length).toBe(1);
